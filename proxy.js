c2d7edb8ce5d751eeaa57bef857d175fa020831a
Mithunkr00808 <84951820+Mithunkr00808@users.noreply.github.com>
2025-10-11 15:41:34 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+let container_id;
+
+
 app.post('/facebook', async (req, res) => {
   try {
     const response = await axios({
@@ -15,10 +18,26 @@ app.post('/facebook', async (req, res) => {
       },
       data: req.body.payload || {},
     });
+    container_id = response.data;
     res.json(response.data);
   } catch (err) {
     res.status(err.response?.status || 500).json({ error: err.message });
   }
 });
+
+app.get('/facebook', async (req, res) => {
+  try {
+    const endpoint = req.query.endpoint;
+    const token = req.query.token;
+    const response = await axios({
+      method: 'GET',
+      url: `https://graph.facebook.com/v22.0/${endpoint}?access_token=${token}&fields=status_code`,
+    });
+    res.json(response.data);
+  } catch (err) {
+    res.status(err.response?.status || 500).json({ error: err.message });
+  }
+});
+
 
 app.listen(3000, () => console.log('Proxy running on port 3000'));
